e3e45b686720d7658f58fc402a1ad06f8397453d
PyeongGangKim <21500185@handong.edu>
2020-03-19 14:19:23 +0900

    global variable

--- assignment/21500185_김평강_hw1.c
+++ assignment/21500185_김평강_hw1.c
@@ -6,26 +6,30 @@ typedef struct Node{
     char element[22];
     int key;
 }node;
-void swap(node* heap, int a, int b){//change the node's position 
+
+node heap[31];
+int heapSize=0;
+
+void swap(int a, int b){//change the node's position 
     node temp= heap[a];
     heap[a]=heap[b];
     heap[b]=temp;
 }
-void increaseKey(node* heap,int i,int key){//increase the value of the selected index. 
+void increaseKey(int i,int key){//increase the value of the selected index. 
     if(key<heap[i].key){
         printf("new key is smaller than current key.\n");
         return;
     }
     heap[i].key=key;
     while(i>1&&(heap[i/2].key)<(heap[i].key)){//execute when index size is less than 1 and current key value is greater than parent's key value.
-        swap(heap,i/2,i);//change position with parent's node.
+        swap(i/2,i);//change position with parent's node.
         i=i/2;
     }
 }
-int insert (node* heap, int size){//insert new node
-    if(size==30){
+void insert (){//insert new node
+    if(heapSize==30){
         printf("queue is full.\n");
-        return size;
+        return;
     }
     int newKey;
     char newElement[22];
@@ -44,46 +48,44 @@ int insert (node* heap, int size){//insert new node
     }
     strcpy(newNode->element,newElement);
     newNode->key=-1;
-    size++;
-    heap[size]=*newNode;//first insert the new node at the end of array.
-    increaseKey(heap,size,newKey);//move the new node to the correct position in the heap.
+    heapSize++;
+    heap[heapSize]=*newNode;//first insert the new node at the end of array.
+    increaseKey(heapSize,newKey);//move the new node to the correct position in the heap.
     printf("New element [%s, %d] is inserted\n",newNode->element,newKey);
-    return size;
+    return;
 }
-void maxHeapify(node* heap,int i,int size){//maintain the max-heap property
+void maxHeapify(int i){//maintain the max-heap property
    int lChild=i*2;//left child index
    int rChild=i*2+1;//right child indx
    int largest;
-   if(lChild<size&&heap[lChild].key>=heap[i].key) largest=lChild;//if the left child exists and is bigger than parent, then put it into the largest.
+   if(lChild<heapSize&&heap[lChild].key>=heap[i].key) largest=lChild;//if the left child exists and is bigger than parent, then put it into the largest.
    else largest=i;
-   if(rChild<size&&heap[rChild].key>=heap[largest].key) largest=rChild;//if the right child exists and is bigger than largest, then put it into the largest. 
+   if(rChild<heapSize&&heap[rChild].key>=heap[largest].key) largest=rChild;//if the right child exists and is bigger than largest, then put it into the largest. 
    if(largest!=i){ 
-       swap(heap,i,largest);
-       maxHeapify(heap,largest,size);//repeat until max-heap is reached
+       swap(i,largest);
+       maxHeapify(largest);//repeat until max-heap is reached
    }
    return;
 }
-int delete(node* heap,int size){
-    if(size<1){
+void delete(){
+    if(heapSize<1){
         printf("heap underflow.\n");
-        return size;
+        return;
     }
     node max=heap[1];
-    heap[1]=heap[size];//replace the first node with the last one.
-    size--;
-    maxHeapify(heap,1,size);
+    heap[1]=heap[heapSize];//replace the first node with the last one.
+    heapSize--;
+    maxHeapify(1);
     printf("[%s, %d] is deleted\n",max.element,max.key);
-    return size;
+    return;
 }
-void printAll(node* heap,int size){//print all node in max-heap
-    for(int i=1;i<=size;i++){
+void printAll(){//print all node in max-heap
+    for(int i=1;i<=heapSize;i++){
         printf("[%s, %d] ",heap[i].element,heap[i].key);
     }
     printf("\n");
 }
 int main(){
-	node heap[31];
-    int heapSize=0;
     char menu;
     int i,newKey;
     while(1){
@@ -100,10 +102,10 @@ int main(){
         scanf("%s",&menu);
         switch (menu){//execute the correct option
             case 'I' :
-                    heapSize=insert(heap,heapSize);
+                    insert();
                     break;
             case 'D' :   
-                    heapSize=delete(heap,heapSize);
+                    delete();
                     break;
             case 'R' :
                     printf("[%s, %d]\n",heap[1].element,heap[1].key);
@@ -118,10 +120,10 @@ int main(){
                     }
                     printf("Enter new key value: ");
                     scanf("%d",&newKey);
-                    increaseKey(heap,i,newKey);
+                    increaseKey(i,newKey);
                     break;
             case 'P' :
-                    printAll(heap,heapSize);
+                    printAll();
                     break;
             case 'Q' : 
                     printf("Thank you. Bye!\n");
